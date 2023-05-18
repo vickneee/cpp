bc641a23bae8263f7e8ce798494d444764e248ce
vickneee <victoria.vavulina@outlook.com>
2023-05-18 10:53:04 +0300

    First Commit

---
+++ tehtavat2.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+int main() {
+    int num5, num6;
+
+        cout << "Anna ensimmainen numero: ";
+        cin >> num5;
+        cout << "Anna toinen numero: ";
+        cin >> num6;
+        cout << num5 << " * " << num6 << " = " << num5 * num6 << endl;
+}
+
